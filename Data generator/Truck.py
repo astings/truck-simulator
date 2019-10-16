5a3ca93fb8e95b7b9fdc55ccb970050ba0a93d95
Alexandre Lainé <alexandre.laine@student.ecp.fr>
2019-10-16 14:05:25 +0200

    Updates truck, creates main

---
+++ Data generator/Truck.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class Truck:
+    count_id = 0
+
+    def __init__(self, speed: int = 80):
+        self.num_id = count_id
+        count_id += 1
+        self.speed = speed
+
+    def generate_itinerary(self, direction):
+        url = "https://openrouteservice.org/"+
+
+    def get_coordinates(self):
+        pass
+
+    def drive_truck(self):
+        pass
