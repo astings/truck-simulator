3723f226b17909fc7975f1234e2218ea4a0e3b31
toinne57520 <a.bour1210@gmail.com>
2019-10-23 15:34:00 +0200

    correction get coordinates

--- data_generator/truck.py
+++ data_generator/truck.py
@@ -5,7 +5,7 @@ import geopy.distance
 from random import uniform
 from geojsonio import display
 import json
-
+import pyproj
 
 class Truck:
     count_id = 0
@@ -16,6 +16,7 @@ class Truck:
         self.speed = speed
         self.distances = []
         self.coord = []
+        self.plan_coord = []
 
     @staticmethod
     def generate_itinerary(start: dict, end: dict):
@@ -34,18 +35,21 @@ class Truck:
         return coordinates
 
     def get_coordinates(self):
+        main_distance = 0
         distance = 0
         L = []
         i=0
-        while distance < sum(self.distances):
+        while main_distance < sum(self.distances):
             while distance > 0 and i < len(self.distances)-1:
                 distance -= self.distances[i]
                 i += 1
-
             distance += self.distances[i-1]
             advancement = distance / self.distances[i-1]
             L.append([self.coord[i-1][0] + advancement * (self.coord[i][0] - self.coord[i-1][0]) ,self.coord[i-1][1] + advancement * (self.coord[i][1] - self.coord[i-1][1])])
-            distance += 1
+            i=0
+            main_distance += 10
+            distance = main_distance
+
         return (L)
 
 
@@ -55,13 +59,28 @@ class Truck:
         distances = []
         for i in range(len(coord) - 1):
             distances.append(geopy.distance.geodesic(coord[i], coord[i + 1]).m)
-            # self.compute_distance(coord[i][0], coord[i][1], coord[i + 1][0], coord[i + 1][1]))
         return distances
 
     @staticmethod
     def compute_distance(lat1, lon1, lat2, lon2):
         return np.arccos(np.sin(math.radians(lat1)) * np.sin(math.radians(lat2)) + np.cos(math.radians(lat1)) * np.cos(
             math.radians(lat2)) * np.cos(math.radians(lon1) - math.radians(lon2))) * 6371
+    @staticmethod
+    def change_coordinate_system(coord):
+        new_coordinate = []
+        isn2004 = pyproj.Proj(
+            "+proj=lcc +lat_1=64.25 +lat_2=65.75 +lat_0=65 +lon_0=-19 +x_0=1700000 +y_0=300000 +no_defs +a=6378137 +rf=298.257222101 +to_meter=1")
+        wgs84 = pyproj.Proj("+init=EPSG:4326")
+        for coordonnee in coord:
+            new_coordinate.append([pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[0],pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[1]])
+        return new_coordinate
+
+    def new_coordinate_distance(self):
+        coord = self.plan_coord
+        distance = []
+        for i in range(len(coord)-1):
+            distance.append(math.sqrt(math.pow(coord[i+1][0]-coord[i][0],2)+math.pow(coord[i+1][1]-coord[i][1],2)))
+        return distance
 
     def drive(self):
         start = {'lng': uniform(2.2632, 2.4083),
@@ -69,12 +88,13 @@ class Truck:
         end = {'lng': uniform(2.2632, 2.4083),
                'lat': uniform(48.8319, 48.9019)}
         self.coord = self.generate_itinerary(start, end)
-        self.distances = self.get_distances(self.coord)
+        self.plan_coord = self.change_coordinate_system(self.coord)
+        self.distances = self.new_coordinate_distance()
         print(sum(self.distances))
 
     def display_geojson(self):
         geo_object2 = {
-            "type": "LineString",
+            "type": "MultiPoint",
             "coordinates": truck.get_coordinates()
         }
         display(json.dumps(geo_object2))
@@ -100,4 +120,6 @@ if __name__ == "__main__":
     print(truck.get_coordinates())
     print(truck.get_coordinates()[-1])
 
-    #truck.display_geojson()
+    truck.display_geojson()
+    print(truck.plan_coord)
+    print(sum(truck.new_coordinate_distance()))
