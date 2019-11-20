7e4da37cbe49137951beee6277b2429f712b2153
Robin Ambert <robin.ambert@student.ecp.fr>
2019-11-20 11:14:42 +0100

    Feature: Speed generation + perf improved

--- data_generator/truck.py
+++ data_generator/truck.py
@@ -1,7 +1,5 @@
 import math
-import numpy as np
 import requests
-import geopy.distance
 from random import uniform
 from geojsonio import display
 import json
@@ -9,6 +7,10 @@ import pyproj
 from flotte import Flotte
 from ors_api import ApiOrs
 from shapely.geometry import Polygon
+from time import time
+from numpy.random import normal
+from operator import itemgetter
+
 
 class Truck:
     count_id = 0
@@ -20,24 +22,31 @@ class Truck:
         self.num_id = Truck.count_id
         Truck.count_id += 1
         self.speed = speed
+        self.saved_call = None
         self.distances = []
         self.coord = []
         self.plan_coord = []
         self.owner = flotte
+        self.speeds = []
         flotte.truck_list.append(self)
 
-
-    @staticmethod
-    def generate_itinerary(start: dict, end: dict):
+    def generate_itinerary(self, start: dict, end: dict):
         call_json = Truck.api_ors.get_direction(start, end)
+        self.saved_call = call_json
+        segments = self.saved_call["features"][0]["properties"]["segments"][0]["steps"]
+        seg_coord_index = []
+        for elt in segments:
+            seg_coord_index += elt['way_points']
+        seg_coord_index = list(set(seg_coord_index))
         coordinates = call_json['features'][0]['geometry']['coordinates']
+        relevant_coordinates = itemgetter(*seg_coord_index)(coordinates)
         return coordinates
 
     def get_coordinates(self):
         main_distance = 0
         distance = 0
         L = []
-        i=0
+        i = 0
         while main_distance < sum(self.distances):
             while distance > 0 and i < len(self.distances)-1:
                 distance -= self.distances[i]
@@ -45,12 +54,11 @@ class Truck:
             distance += self.distances[i-1]
             advancement = distance / self.distances[i-1]
             L.append([self.coord[i-1][0] + advancement * (self.coord[i][0] - self.coord[i-1][0]) ,self.coord[i-1][1] + advancement * (self.coord[i][1] - self.coord[i-1][1])])
-            i=0
+            i = 0
             main_distance += 10
             distance = main_distance
 
         return (L)
-
 
     @staticmethod
     def change_coordinate_system(coord):
@@ -58,9 +66,12 @@ class Truck:
         isn2004 = pyproj.Proj(
             "+proj=lcc +lat_1=64.25 +lat_2=65.75 +lat_0=65 +lon_0=-19 +x_0=1700000 +y_0=300000 +no_defs +a=6378137 +rf=298.257222101 +to_meter=1")
         wgs84 = pyproj.Proj("+init=EPSG:4326")
-        for coordonnee in coord:
-            new_coordinate.append([pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[0],pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[1]])
-        return new_coordinate
+        transformer = pyproj.Transformer.from_proj(wgs84, isn2004)
+        new_coordinate = list(map(list, zip(*coord)))
+        result = transformer.transform(new_coordinate[0], new_coordinate[1])
+        # for coordonnee in coord:
+        #     new_coordinate.append([pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[0],pyproj.transform(wgs84, isn2004, coordonnee[0], coordonnee[1])[1]])
+        return list(map(list,zip(*result)))
 
     @staticmethod
     def get_distance(coord):
@@ -76,36 +87,62 @@ class Truck:
         idf_polygon = Polygon(idf_coords)
         min_x, min_y, max_x, max_y = idf_polygon.bounds[0], idf_polygon.bounds[1], idf_polygon.bounds[2], idf_polygon.bounds[3]
         point = {
-            "lng" : uniform(min_x, max_x),
-            "lat" : uniform(min_y, max_y)
+            "lng": uniform(min_x, max_x),
+            "lat": uniform(min_y, max_y)
         }
         return point
+    
+    def generate_speeds(self, base_variation):
+        if self.saved_call is None:
+            raise ValueError("No previous call made.")
+        segments = self.saved_call["features"][0]["properties"]["segments"][0]["steps"]
+        speed = self.speed
+        speeds = list()
+        for segment in segments:
+            speed += base_variation * normal(0, 0.5, 1)[0]
+            nb_coord_points = segment["way_points"][1] - segment["way_points"][0] + 1
+            speeds.extend([speed] * nb_coord_points)
+        return speeds
 
-    def drive(self):
+    def drive(self, debug: bool = False):
+        start_time = time()
         start = self.generate_random_point()
         end = self.generate_random_point()
+        generate_points_time = time()
         self.coord = self.generate_itinerary(start, end)
+        generate_itinerary_time = time()
         self.plan_coord = self.change_coordinate_system(self.coord)
+        change_coord_sys_time = time()
         self.distances = self.get_distance(self.plan_coord)
-        print(sum(self.distances))
+        calc_dist_time = time()
+        self.speeds = self.generate_speeds(1.5)
+        generate_speeds_time = time()
+        if debug:
+            print('Time to:\n')
+            print('Generate points: %f'%(generate_points_time - start_time))
+            print('Generate itinerary: %f'%(generate_itinerary_time - generate_points_time))
+            print('Change coord: %f'%(change_coord_sys_time - generate_itinerary_time))
+            print('Calculate distances: %f'%(calc_dist_time - change_coord_sys_time))
+            print('Generate speeds: %f'%(generate_speeds_time - calc_dist_time))
+            print('____________________________')
+            print('TOTAL : %f'%(generate_speeds_time - start_time))
+            print(len(self.coord))
+        return(sum(self.distances))
 
     def display_geojson(self):
         geo_object2 = {
             "type": "Feature",
             "geometry": {
                 "type": "MultiPoint",
-                "coordinates": self.get_coordinates()
+                "coordinates": self.coord
             },
-            "properties":{
-                "name": "Truck %i"% self.num_id
+            "properties": {
+                "name": "Truck %i" % self.num_id
             }
 
         }
 
         display(json.dumps(geo_object2))
-
-
-
 
 
 if __name__ == "__main__":
@@ -116,7 +153,12 @@ if __name__ == "__main__":
     #     truck.drive()
     #
     # flotte.display_geojson()
+    # help(pyproj.Transformer)
     f1 = Flotte()
     t1 = Truck(flotte=f1)
-    t1.drive()
-    t1.display_geojson()
+    for _ in range(15):
+        dist = t1.drive(debug = False)
+        print("Relative error:")
+        print((t1.saved_call['features'][0]['properties']['segments'][0]['distance']- dist)/dist)
+        print('\n Abs error:')
+        print(t1.saved_call['features'][0]['properties']['segments'][0]['distance']- dist)
