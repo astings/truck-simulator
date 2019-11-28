fc267b4e03bb080ec623f9c7c6d327a2c051377e
Robin Ambert <robin.ambert@student.ecp.fr>
2019-11-28 14:34:21 +0100

    Reformat: Capitalized constants

--- data_generator/config.py
+++ data_generator/config.py
@@ -1,7 +1,7 @@
 from shapely.geometry import Polygon
 import pyproj
 
-idf_polygon = Polygon(
+IDF_POLYGON = Polygon(
     [[2.3198871747441, 48.90045978209],
      [2.3851496429397, 48.902007785215],
      [2.394906293421, 48.898444039523],
@@ -34,9 +34,20 @@ idf_polygon = Polygon(
 
 )
 
-isn2004 = pyproj.Proj(
+ISN2004 = pyproj.Proj(
             "+proj=lcc +lat_1=64.25 +lat_2=65.75 +lat_0=65 +lon_0=-19 + x_0=1700000 +y_0=300000 +no_defs +a=6378137 +rf=298.257222101 +to_meter=1")
 
-wgs84 = pyproj.Proj("+init=EPSG:4326")
+WGS84 = pyproj.Proj("+init=EPSG:4326")
 
-ors_key = "5b3ce3597851110001cf6248f022aa2ca82b4338a12afb1b17fbd16f"
+ORS_URL = (
+            "https://api.openrouteservice.org/v2/directions/" +
+            "driving-car?api_key=" +
+            "5b3ce3597851110001cf6248f022aa2ca82b4338a12afb1b17fbd16f"
+            )
+
+ORS_HEADERS = {
+            "Accept": ("application/json, application/geo+json," +
+                       "application/gpx+xml, img/png; charset=utf-8"),
+            "Authorization": (
+                "5b3ce3597851110001cf6248f022aa2ca82b4338a12afb1b17fbd16f")
+        }
