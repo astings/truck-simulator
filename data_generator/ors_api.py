6b6eddf70549169499615f7ba097aabd1f812d94
Robin Ambert <robin.ambert@student.ecp.fr>
2019-11-28 14:36:09 +0100

    Refactor: class transformed to function

--- data_generator/ors_api.py
+++ data_generator/ors_api.py
@@ -1,49 +1,28 @@
-"""Openrouteservice API class to perform API calls in the Truck class."""
+"""Openrouteservice API function to perform API calls in the Truck class."""
 
 import requests
-from config import ors_key
+from config import ORS_URL, ORS_HEADERS
 
 
-class ApiOrs():
-    """
-    Openrouteservice Api object, used to fetch direction between two points
+def get_direction(start: dict, end: dict):
+    """Make API call to OpenRouteService API to generate directions.
 
     Args:
+        start (dict): Starting point, contains keys lat and lng with
+        matching lattitude and longitude of point.
+        end (dict): End point, contains keys lat and lng with
+        matching lattitude and longitude of point.
 
-    Attributes:
-        _key (str): API key
-        _base_url (str): URL used to make requests
-        _headers (dict): Header passed in the request
+    Returns:
+        dict: JSON associated with returned call
     """
-    def __init__(self):
-        self._key = ors_key
-        self._base_url = (
-            "https://api.openrouteservice.org/v2/directions/" +
-            "driving-car?api_key=" + self._key
-            )
-        self._headers = {
-            "Accept": ("application/json, application/geo+json," +
-                       "application/gpx+xml, img/png; charset=utf-8"),
-            "Authorization": self._key,
-        }
 
-    def get_direction(self, start: dict, end: dict):
-        """
-        Method invoked to make a direction GET request to the API
+    params = {
+        "start": ",".join(map(str, [start['lng'], start['lat']])),
+        "end": ",".join(map(str, [end['lng'], end['lat']]))
+    }
+    call = requests.get(ORS_URL,
+                        params=params,
+                        headers=ORS_HEADERS)
 
-        Args:
-            start (dict): Starting point, with keys 'lng' and 'lat'
-            end (dict): Ending point, with keys 'lng' and 'lat'
-
-        Returns:
-            call (dict): JSON formatted object returned by the call
-        """
-        params = {
-            "start": ",".join(map(str, [start['lng'], start['lat']])),
-            "end": ",".join(map(str, [end['lng'], end['lat']]))
-        }
-        call = requests.get(self._base_url,
-                            params=params,
-                            headers=self._headers)
-
-        return call.json()
+    return call.json()
