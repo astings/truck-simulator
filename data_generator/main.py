85fd204e3a04461b0163a53318d0f4aac37d0af7
Robin Ambert <robin.ambert@student.ecp.fr>
2019-11-28 14:35:00 +0100

    Refactor: class becomes script

--- data_generator/main.py
+++ data_generator/main.py
@@ -1,34 +1,37 @@
-from flotte import Flotte
 from truck import Truck
 import pandas as pd
+import sys
+
+id = 0
 
 
-class Main():
-    def __init__(self):
-        self._writer = Csv_Writer()
+def write(n):
+    """Write data locally for n simulations.
 
-    def write(self, n):
-        """Write data locally for n simulations."""
-        for _ in range(n):
-            self._writer.run()
+    Args:
+        n (int): number of simulations ran
+    """
+    for _ in range(n):
+        run()
 
 
-class Csv_Writer():
-    def __init__(self):
-        self.id = 0
-        self.flotte = Flotte()
-        self.truck = Truck(self.flotte)
+def run():
+    """"Write generated data into a local csv file."""
+    global id
+    truck = Truck(id=0)
+    truck.drive()
+    name = r'csv_files/test' + str(id) + '.csv'
+    id += 1
+    result = list(map(list, zip(*truck.get_coordinates())))
+    df = pd.DataFrame({
+        'lng': result[0],
+        'lat': result[1],
+        'ts': result[2],
+        'speed': result[3]
+    })
+    df.to_csv(name, header=True)
 
-    def run(self):
-        """Write generated data into a local csv file."""
-        self.truck.drive()
-        name = r'data_generator/csv_files/test' + str(self.id) + '.csv'
-        self.id += 1
-        result = list(map(list, zip(*self.truck.get_coordinates())))
-        df = pd.DataFrame({
-            'lng': result[0],
-            'lat': result[1],
-            'ts': result[2],
-            'speed': result[3]
-        })
-        df.to_csv(name, header=True)
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    write(n)
