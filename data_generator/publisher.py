ec529dc33550a8f3d4c365852137d03d8fe1e6b6
Robin Ambert <robin.ambert@student.ecp.fr>
2020-01-15 15:52:18 +0100

    first draft consumer/producer

--- data_generator/publisher.py
+++ data_generator/publisher.py
@@ -1,17 +1,48 @@
 import pika
 import json
+import time
+from truck import Truck
+from random import randint
 
-def emit_journey_info(truck):
+
+def emit_message(payload):
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq-server")
+        pika.ConnectionParameters(host="localhost")
         )
     channel = connection.channel()
 
-    exchange_name = 'journey'
-    routing_key = 'journey.new'
+    channel.queue_declare(queue='exchange', durable=True)
+    channel.basic_publish(exchange='',
+                          routing_key='exchange',
+                          body=json.dumps(payload),
+                          )
+    print(" [x] Message publisher %r" % payload)
+    connection.close()
 
-    channel.exchange_declare(
-        exchange=exchange_name,
-        exchange_type="topic",
-        durable=True
-    )
+
+def emit_truck_info():
+    id_truck = randint(0, 100)
+    id_driver = randint(0, 100)
+    id_itinerary = randint(0, 100)
+    truck = Truck(id_truck)
+    departure, arrival = truck.drive()
+    journey_ended = False
+    t = 0
+    while not journey_ended:
+        position = truck.get_position_at_time(t)
+        payload = {"position":
+                   {"id_driver": id_driver,
+                        "id_truck": id_truck,
+                        "status": 0,
+                        "id_itinerary": id_itinerary,
+                        "position": position
+                   }
+                  }
+        journey_ended = position == arrival
+        emit_message(payload)
+        t += 1
+        time.sleep(1)
+
+
+while True:
+    emit_truck_info()
