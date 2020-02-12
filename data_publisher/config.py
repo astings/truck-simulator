680fd9c439da5ed53afd82b60029455730bafe3b
toinne57520 <a.bour1210@gmail.com>
2020-02-12 10:55:44 +0100

    change config file for pyproj

--- data_publisher/config.py
+++ data_publisher/config.py
@@ -37,7 +37,8 @@ IDF_POLYGON = Polygon(
 ISN2004 = pyproj.Proj(
             "+proj=lcc +lat_1=64.25 +lat_2=65.75 +lat_0=65 +lon_0=-19 + x_0=1700000 +y_0=300000 +no_defs +a=6378137 +rf=298.257222101 +to_meter=1")
 
-WGS84 = pyproj.Proj("+init=EPSG:4326")
+#WGS84 = pyproj.Proj("+init=EPSG:4326")
+WGS84 = pyproj.Proj("EPSG:4326")
 
 ORS_URL = (
             "https://api.openrouteservice.org/v2/directions/" +
